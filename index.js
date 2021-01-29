eb2570d105a645ccfd3425283354a123b1f4c817
JoaquinGomez1 <joaquingomez3098@gmail.com>
2021-01-29 16:41:21 -0300

    changed CORS origin whitelist

--- index.js
+++ index.js
@@ -58,7 +58,9 @@ app.use(
   })
 );
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({ origin: "https://electroniks.netlify.app/", credentials: true })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
