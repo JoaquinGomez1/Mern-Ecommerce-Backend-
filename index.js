2014c9cc8919cff27b6af786768aa20962653201
JoaquinGomez1 <joaquingomez3098@gmail.com>
2021-02-20 12:44:44 -0300

    Changed cors origin

--- index.js
+++ index.js
@@ -57,25 +57,7 @@ app.use(
     },
   })
 );
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://electroniks.netlify.app/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization"
-  );
-  next();
-});
-
-app.use(
-  cors({ origin: "https://electroniks.netlify.app/", credentials: true })
-);
+app.use(cors({ origin: "https://electroniks.netlify.app", credentials: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
